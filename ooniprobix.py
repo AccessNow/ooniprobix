9c4b84c58538a8e3e8da00e54959ec8953159123
jdoe <jdoe@localhost.localdomain>
2013-07-10 18:02:50 -0400

    Added menu and status bars

--- ooniprobix.py
+++ ooniprobix.py
@@ -1,6 +1,29 @@
 import wx
 
+authors = 'Peter Bourgelais'
+version_number = '0.0.1'
+
+class ProbixMainWindow(wx.Frame):
+	def __init__(self, parent, title):
+		wx.Frame.__init__(self,parent,title=title,size=(800,600))
+
+		#TO-DO: Will need for basic logging
+		self.CreateStatusBar()
+
+		filemenu = wx.Menu()
+		filemenu.Append(wx.ID_ABOUT,'&About','OONIProbix version ' + version_number + ' by ' + authors)
+		filemenu.AppendSeparator()
+		filemenu.Append(wx.ID_EXIT,'&Exit','Exit OONIProbix')
+	
+		menuBar = wx.MenuBar()
+		menuBar.Append(filemenu,'&File')
+	
+		self.SetMenuBar(menuBar)
+	
+		self.Show(True)
+
+
+
 app = wx.App(False)
-frame = wx.Frame(None, wx.ID_ANY, "OONIProbix")
-frame.Show(True)
+frame = ProbixMainWindow(None, 'OONIProbix ' + version_number)
 app.MainLoop()
